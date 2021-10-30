b2129b75af2e3d78078f8e3eedeb51f28c200450
GenosW <58387550+GenosW@users.noreply.github.com>
2021-10-30 17:31:48 +0200

    ex1 finish

--- homework/ex1/generate_cleaned_tokens.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat data/alice_tok_de_original.txt | sed 's/^$/@/' | tr '\n' ' ' | sed 's/ @ /\n/g' | \
-sed -E "s/\[ ?[0-9]+ ?\] //" | \
-cut -d' ' -f2- | tr ' ' '\n' | \
-sed 's/^[ 0-9]*//' | comm -13 data/stopwords_de.txt - > data/alice_tok_de_.txt
--- homework/ex1/simple_entities.sh
+++ homework/ex1/simple_entities.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # extract capitalized words, count them, reformat, save to file: 
-cat data/alice_tok_de.txt | grep -E '^[A-Z][a-z]+' | sort | uniq -c | sort -nr | sed 's/^ *\([0-9]*\) \(.*\)$/\2\t\1/' | tr [:upper:] [:lower:] | sort > out/ent_freqs.txt
+cat alice_tok_de.txt | grep -E '^[A-Z][a-z]+' | sort | uniq -c | sort -nr | sed 's/^ *\([0-9]*\) \(.*\)$/\2\t\1/' | tr [:upper:] [:lower:] | sort > ent_freqs.txt
 
 # reformat to get one sentence per line, 
 # get rid of the bracketed numbers: [ xx ] (e.g. [1] before 'Erstes Kapitel'),
@@ -10,10 +10,10 @@ cat data/alice_tok_de.txt | grep -E '^[A-Z][a-z]+' | sort | uniq -c | sort -nr |
 # and then only those that are not in the stopwords file, 
 # finally match the lines to the lines of the word frequency file and sort by frequency
 # and write the output to a file (OPTIONAL)
-cat data/alice_tok_de.txt | sed 's/^$/@/' | tr '\n' ' ' | sed 's/ @ /\n/g' | \
+cat alice_tok_de.txt | sed 's/^$/@/' | tr '\n' ' ' | sed 's/ @ /\n/g' | \
 sed -E "s/\[ ?[0-9]+ ?\] //" | \
 cut -d' ' -f2- | tr ' ' '\n' | \
 grep -E '^[A-Z][a-z]+' | sort | uniq -c | sort -nr | head -50 | \
-sed 's/^[ 0-9]*//' | sort | tr [:upper:] [:lower:] | comm -13 data/stopwords_de.txt - | \
-join - out/ent_freqs.txt | sort -k2 -nr # > \
-# out/result_german.txt
+sed 's/^[ 0-9]*//' | sort | tr [:upper:] [:lower:] | comm -13 stopwords_de.txt - | \
+join - ent_freqs.txt | sort -k2 -nr #> \
+# result_german.txt
